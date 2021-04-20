2e17ee55d880e769a4b32b2ed635be58f8981fe5
Badisi <badisi84@gmail.com>
2021-04-20 12:13:34 +0200

    feat(jasmine): improve schematic tests + add reporter

--- projects/schematics/src/ng-add/index.spec.ts
+++ projects/schematics/src/ng-add/index.spec.ts
@@ -6,44 +6,63 @@ import { join } from 'path';
 
 import { Schema as SchematicOptions } from '../schema/schema.model';
 
+const workspaceOptions: WorkspaceOptions = {
+    name: 'workspace',
+    newProjectRoot: 'projects',
+    version: '0.0.0'
+};
+
+const appOptions: ApplicationOptions = {
+    name: 'app-test',
+    inlineStyle: false,
+    inlineTemplate: false,
+    routing: false,
+    style: Style.Scss,
+    skipTests: false,
+    skipPackageJson: false
+};
+
+const schematicOptions: SchematicOptions = {
+    arg1: ''
+};
+
+const collectionPath = join(__dirname, '../collection.json');
+
+const runner = new SchematicTestRunner('ngAdd schematic', collectionPath);
+
+const getCleanAppTree = async (): Promise<UnitTestTree> => {
+    const workspaceTree = await runner
+        .runExternalSchematicAsync('@schematics/angular', 'workspace', workspaceOptions)
+        .toPromise();
+    return await runner
+        .runExternalSchematicAsync('@schematics/angular', 'application', appOptions, workspaceTree)
+        .toPromise();
+};
+
 describe('Test - ngAdd schematic', () => {
-    const collectionPath = join(__dirname, '../collection.json');
-    const runner = new SchematicTestRunner('ngAdd schematic', collectionPath);
-    let appTree: UnitTestTree;
-
-    const workspaceOptions: WorkspaceOptions = {
-        name: 'workspace',
-        newProjectRoot: 'projects',
-        version: '0.0.0'
-    };
-
-    const appOptions: ApplicationOptions = {
-        name: 'app-test',
-        inlineStyle: false,
-        inlineTemplate: false,
-        routing: false,
-        style: Style.Scss,
-        skipTests: false,
-        skipPackageJson: false
-    };
-
-    const schematicOptions: SchematicOptions = {
-        arg1: ''
-    };
-
-    /**
-     * Create angular application before each test
-     */
-    beforeEach(async () => {
-        appTree = await runner.runExternalSchematicAsync('@schematics/angular', 'workspace', workspaceOptions).toPromise();
-        appTree = await runner.runExternalSchematicAsync('@schematics/angular', 'application', appOptions, appTree).toPromise();
-    });
-
     /**
      * Angular project should be required
      */
     it('should failed without an angular app', async () => {
         const tree$ = runner.runSchematicAsync('ng-add', schematicOptions, Tree.empty()).toPromise();
         await expectAsync(tree$).toBeRejected();
+    });
+
+    /**
+     * Schematics should work
+     */
+    describe('should update & create files', () => {
+        let tree: UnitTestTree;
+        let nbFiles: number;
+
+        beforeAll(async () => {
+            tree = await getCleanAppTree();
+            nbFiles = tree.files.length;
+            tree = await runner.runSchematicAsync('ng-add', schematicOptions, tree).toPromise();
+        });
+
+        it('should not create any new files', () => {
+            expect(tree.files.length).toEqual(nbFiles);
+        });
     });
 });
